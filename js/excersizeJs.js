1411fea62608564b0da5c3be697f2b40c2ee78ab
Kmichal1201 <mkordasinski@gmail.com>
2023-03-25 22:44:22 +0100

    commit6

---
+++ js/excersizeJs.js
@@ -0,0 +1,10 @@
+/*jslint devel: true */
+console.log("tetx1");
+var imie = "michal";
+console.log(imie);
+let tab1 = ["michal","iga","emma"];
+console.log(tab1);
+function sayHello(){
+    console.log("hello");
+}
+sayHello();
